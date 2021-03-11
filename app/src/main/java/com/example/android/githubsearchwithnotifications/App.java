c7f833ae5552ea8f121cab7edb3633f54aa57524
Rob Hess <hessro@oregonstate.edu>
2021-03-11 15:28:15 -0800

    Add code from 3/11 lecture.

--- app/src/main/java/com/example/android/githubsearchwithnotifications/App.java
+++ app/src/main/java/com/example/android/githubsearchwithnotifications/App.java
@@ -1,13 +1,18 @@
 package com.example.android.githubsearchwithnotifications;
 
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.util.Log;
 
 import java.util.concurrent.TimeUnit;
 
 import androidx.work.Constraints;
 import androidx.work.ExistingPeriodicWorkPolicy;
+import androidx.work.ExistingWorkPolicy;
 import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
 import androidx.work.PeriodicWorkRequest;
 import androidx.work.WorkManager;
 
@@ -18,6 +23,7 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        createStarsNotificationChannel();
         startCheckStarsWorker();
     }
 
@@ -27,17 +33,35 @@ public class App extends Application {
                 .setRequiredNetworkType(NetworkType.CONNECTED)
                 .build();
 
-        PeriodicWorkRequest workRequest = new PeriodicWorkRequest.Builder(
-                CheckRepoStarsWorker.class,
-                15,
-                TimeUnit.MINUTES
-        ).setConstraints(constraints).build();
+//        PeriodicWorkRequest workRequest = new PeriodicWorkRequest.Builder(
+//                CheckRepoStarsWorker.class,
+//                15,
+//                TimeUnit.MINUTES
+//        ).setConstraints(constraints).build();
+//
+//        WorkManager.getInstance(this)
+//                .enqueueUniquePeriodicWork(
+//                        CHECK_STARS_WORK_NAME,
+//                        ExistingPeriodicWorkPolicy.KEEP,
+//                        workRequest
+//                );
 
-        WorkManager.getInstance(this)
-                .enqueueUniquePeriodicWork(
-                        CHECK_STARS_WORK_NAME,
-                        ExistingPeriodicWorkPolicy.KEEP,
-                        workRequest
-                );
+        OneTimeWorkRequest workRequest = new OneTimeWorkRequest.Builder(CheckRepoStarsWorker.class)
+                .build();
+        WorkManager.getInstance(this).enqueue(
+                workRequest
+        );
+    }
+
+    private void createStarsNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    getString(R.string.stars_notification_channel),
+                    getString(R.string.stars_notification_channel_title),
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
--- app/src/main/java/com/example/android/githubsearchwithnotifications/CheckRepoStarsWorker.java
+++ app/src/main/java/com/example/android/githubsearchwithnotifications/CheckRepoStarsWorker.java
@@ -1,7 +1,10 @@
 package com.example.android.githubsearchwithnotifications;
 
 import android.app.Application;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.example.android.githubsearchwithnotifications.data.BookmarkedReposRepository;
@@ -9,9 +12,13 @@ import com.example.android.githubsearchwithnotifications.data.GitHubRepo;
 import com.example.android.githubsearchwithnotifications.data.GitHubService;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.app.TaskStackBuilder;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 import retrofit2.Call;
@@ -19,6 +26,7 @@ import retrofit2.Response;
 
 public class CheckRepoStarsWorker extends Worker {
     private static final String TAG = CheckRepoStarsWorker.class.getSimpleName();
+    private static final String STARS_NOTIFICATION_GROUP = "starsNotificationGroup";
 
     private BookmarkedReposRepository bookmarkedReposRepository;
     private GitHubService gitHubService;
@@ -32,16 +40,22 @@ public class CheckRepoStarsWorker extends Worker {
     @NonNull
     @Override
     public Result doWork() {
+        Log.d(TAG, "running check stars worker");
         List<GitHubRepo> bookmarkedRepos = this.bookmarkedReposRepository.getAllBookmarkedReposSync();
+        ArrayList<GitHubRepo> updatedRepos = new ArrayList<>();
         if (bookmarkedRepos != null) {
             for (GitHubRepo bookmarkedRepo : bookmarkedRepos) {
                 GitHubRepo updatedRepo = fetchRepoByUrlSync(bookmarkedRepo.url);
                 if (updatedRepo != null) {
                     Log.d(TAG, updatedRepo.fullName + " updated with stars: " + updatedRepo.stars);
                     this.bookmarkedReposRepository.updateBookmarkedRepo(updatedRepo);
+                    if (updatedRepo.stars >= bookmarkedRepo.stars) {
+                        updatedRepos.add(updatedRepo);
+                    }
                 }
             }
         }
+        sendNotifications(updatedRepos);
         return Result.success();
     }
 
@@ -61,5 +75,93 @@ public class CheckRepoStarsWorker extends Worker {
             Log.d(TAG, "error fetching repo with URL: " + response.toString());
             return null;
         }
+    }
+
+    private void sendNotifications(@NonNull List<GitHubRepo> repos) {
+        Context context = getApplicationContext();
+        for (GitHubRepo repo : repos) {
+            sendIndividualNotification(repo, context);
+        }
+        if (repos.size() > 1) {
+            sendSummaryNotification(repos, context);
+        }
+    }
+
+    private void sendIndividualNotification(GitHubRepo repo, Context context) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(
+                context,
+                context.getString(R.string.stars_notification_channel)
+        );
+        builder.setSmallIcon(R.drawable.ic_github)
+                .setContentTitle(context.getString(
+                        R.string.stars_notification_title, repo.fullName
+                ))
+                .setContentText(context.getString(
+                        R.string.stars_notification_text, repo.fullName, repo.stars
+                ))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setGroup(STARS_NOTIFICATION_GROUP);
+
+        Intent intent = new Intent(context, RepoDetailActivity.class);
+        intent.putExtra(RepoDetailActivity.EXTRA_GITHUB_REPO, repo);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addNextIntentWithParentStack(intent);
+
+        PendingIntent pendingIntent = stackBuilder.getPendingIntent(
+                0,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+
+        builder.setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(
+                repo.fullName.hashCode(),
+                builder.build()
+        );
+    }
+
+    private void sendSummaryNotification(List<GitHubRepo> repos, Context context) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(
+                context,
+                context.getString(R.string.stars_notification_channel)
+        );
+        builder.setSmallIcon(R.drawable.ic_github);
+
+        ArrayList<String> repoNames = new ArrayList<>();
+        for (GitHubRepo repo : repos) {
+            repoNames.add(repo.fullName);
+        }
+
+        builder.setContentText(TextUtils.join(", ", repoNames))
+            .setContentTitle(context.getString(
+                    R.string.stars_notification_summary_title, repos.size()
+            ));
+
+        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+        for (GitHubRepo repo : repos) {
+            inboxStyle.addLine(context.getString(
+                    R.string.stars_notification_text, repo.fullName, repo.stars
+            ));
+        }
+        builder.setStyle(inboxStyle);
+
+        Intent intent = new Intent(context, BookmarkedRepos.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addNextIntentWithParentStack(intent);
+        PendingIntent pendingIntent = stackBuilder.getPendingIntent(
+                0,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+
+        builder.setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .setGroup(STARS_NOTIFICATION_GROUP)
+                .setGroupSummary(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(0, builder.build());
     }
 }
